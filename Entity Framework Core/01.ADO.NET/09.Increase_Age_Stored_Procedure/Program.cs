37d49278757b52f0e0be4c60670fb535362c45b6
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-08-26 14:49:17 +0300

    Add files via upload

---
+++ Entity Framework Core/01.ADO.NET/09.Increase_Age_Stored_Procedure/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _09.Increase_Age_Stored_Procedure
+{
+    class Program
+    {
+        const string connectionStringDBMinions = "Server=.;Integrated Security=true;Database=MinionsDB;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+            int minionId = int.Parse(Console.ReadLine());
+            using (var sqlConnection = new SqlConnection(connectionStringDBMinions))
+            {
+                sqlConnection.Open();
+                string increaseAgeProcedureQuery = "EXEC usp_GetOlder @id";
+                using (var increaseAgeProcedureCommand = new SqlCommand(increaseAgeProcedureQuery, sqlConnection))
+                {
+                    increaseAgeProcedureCommand.Parameters.AddWithValue("@id", minionId);
+                    increaseAgeProcedureCommand.ExecuteNonQuery();
+                }
+
+                string resultQuery = "SELECT Name, Age FROM Minions WHERE Id = @Id";
+                using (var resultQueryCommand = new SqlCommand(resultQuery, sqlConnection))
+                {
+                    resultQueryCommand.Parameters.AddWithValue("@id", minionId);
+                    var reader = resultQueryCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} – {reader["Age"]} years old");
+                    }
+                }
+            }
+        }
+    }
+}
