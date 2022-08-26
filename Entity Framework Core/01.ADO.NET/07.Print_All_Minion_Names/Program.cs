8ab47e5062d68d08e6cc159155d777dcbc5e90f2
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-08-26 14:42:59 +0300

    Add files via upload

---
+++ Entity Framework Core/01.ADO.NET/07.Print_All_Minion_Names/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _07.Print_All_Minion_Names
+{
+    class Program
+    {
+        const string connectionStringDBMinions = "Server=.;Integrated Security=true;Database=MinionsDB;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+            using (var sqlConnection = new SqlConnection(connectionStringDBMinions))
+            {
+                sqlConnection.Open();
+                string minionsNameQuery = @"SELECT Name FROM Minions";
+                List<string> minionsNames = new List<string>();
+                
+                using (var minionsNamesCommand = new SqlCommand(minionsNameQuery, sqlConnection))
+                {
+                    using (var reader = minionsNamesCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string minionName = (string)reader["Name"];
+                            minionsNames.Add(minionName);
+                        }
+
+                        for (int i = 0; i < minionsNames.Count / 2; i++)
+                        {
+                            Console.WriteLine(minionsNames[i]);
+                            Console.WriteLine(minionsNames[minionsNames.Count - 1 - i]);
+                        }
+                        if (minionsNames.Count % 2 == 1)
+                        {
+                            Console.WriteLine(minionsNames[minionsNames.Count / 2]);
+                        }
+                    }
+                }
+            }
+         
+        }
+    }
+}
