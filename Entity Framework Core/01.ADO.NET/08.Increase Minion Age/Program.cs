a605b8eef633a5cb25d8eb286f6e1d575715a8fc
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-08-26 14:43:59 +0300

    Add files via upload

---
+++ Entity Framework Core/01.ADO.NET/08.Increase Minion Age/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Linq;
+
+namespace _08.Increase_Minion_Age
+{
+    class Program
+    {
+        const string connectionStringDBMinions = "Server=.;Integrated Security=true;Database=MinionsDB;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+            int[] minionsIds = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            using (var sqlConnection = new SqlConnection(connectionStringDBMinions))
+            {
+                sqlConnection.Open();
+                foreach (int minionId in minionsIds)
+                {
+                    string minionsUpdateQuery = @"UPDATE Minions
+                                                 SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                                                 WHERE Id = @Id";
+                    using (var updateQueryCommand = new SqlCommand(minionsUpdateQuery, sqlConnection))
+                    {
+                        updateQueryCommand.Parameters.AddWithValue("@Id", minionId);
+                        updateQueryCommand.ExecuteNonQuery();
+                    }
+                }
+                string minionResultQuery = "SELECT Name, Age FROM Minions";
+                using (var resultCommand = new SqlCommand(minionResultQuery, sqlConnection))
+                {
+                    using (var reader = resultCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["Name"]} {reader["Age"]}");
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
