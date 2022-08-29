3b48743ab6f614cd90c72761bde8e7b9b3a95355
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-08-29 09:36:59 +0300

    Add files via upload

---
+++ Entity Framework Core/04.EntityRelations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+       public StudentSystemContext(DbContextOptions dbContextOptions) 
+            : base(dbContextOptions)
+        {
+        }
+
+        public virtual DbSet<Course> Courses{ get; set; }
+        public virtual DbSet<Homework> HomeworkSubmissions { get; set; }
+        public virtual DbSet<Resource> Resources { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(x => new { x.CourseId, x.StudentId });
+        }
+      
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Integrated Security=True;Database=StudentSystem;");
+            }
+        }
+    }
+
+}
