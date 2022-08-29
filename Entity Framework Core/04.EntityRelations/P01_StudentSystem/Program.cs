d7fe167ff8a88d1cee1fa113e9ef5477fcaef4b5
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-08-29 09:37:42 +0300

    Add files via upload

---
+++ Entity Framework Core/04.EntityRelations/P01_StudentSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using P01_StudentSystem.Data;
+using System;
+
+namespace P01_StudentSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var myContext = new StudentSystemContext();
+            myContext.Database.EnsureDeleted();
+            myContext.Database.EnsureCreated();
+        }
+    }
+}
