3727fe6ff03c32c69de3f8729d271aadb60d77ca
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-08 15:28:04 +0300

    Add files via upload

---
+++ Entity Framework Core/09.XMLProcessing/ProductShop/DTO/InputModels/CategoryProductInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Import
+{
+    [XmlType("CategoryProduct")]
+    public class CategoryProductInputModel
+    {
+        [XmlElement("CategoryId")]
+        public int CategoryId { get; set; }
+
+        [XmlElement("ProductId")]
+        public int ProductId { get; set; }
+    }
+}
+//< CategoryProduct >
+//        < CategoryId > 6 </ CategoryId >
+//        < ProductId > 3 </ ProductId >
+//</ CategoryProduct >
