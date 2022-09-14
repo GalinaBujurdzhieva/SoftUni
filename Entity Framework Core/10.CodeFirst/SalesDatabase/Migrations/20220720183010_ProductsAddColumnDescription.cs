91fee52ef9988421722ca5bc63dd45991ea9d7ce
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-14 16:48:29 +0300

    Add files via upload

---
+++ Entity Framework Core/10.CodeFirst/SalesDatabase/Migrations/20220720183010_ProductsAddColumnDescription.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_SalesDatabase.Migrations
+{
+    public partial class ProductsAddColumnDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true,
+                oldDefaultValue: "No description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                defaultValue: "No description",
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+    }
+}
