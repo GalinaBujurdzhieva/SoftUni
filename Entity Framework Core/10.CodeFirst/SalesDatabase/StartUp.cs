b262397168c444a278dec04610fad1e76496cd09
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-14 16:49:03 +0300

    Add files via upload

---
+++ Entity Framework Core/10.CodeFirst/SalesDatabase/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data;
+using System;
+
+namespace P03_SalesDatabase
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var dbContext = new SalesContext();
+            dbContext.Database.Migrate();
+        }
+    }
+}
