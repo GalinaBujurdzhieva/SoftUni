54b072f5d072882d48ffca36543d721d27d24e44
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-14 16:45:21 +0300

    Add files via upload

---
+++ Entity Framework Core/10.CodeFirst/HospitalDatabase/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data;
+using System;
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new HospitalContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            
+        }
+    }
+}
