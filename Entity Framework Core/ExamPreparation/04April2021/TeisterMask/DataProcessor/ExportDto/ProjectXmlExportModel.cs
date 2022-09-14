931f166db679339988071a0c40dabd6e65b7e1b2
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-14 16:59:56 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/04April2021/TeisterMask/DataProcessor/ExportDto/ProjectXmlExportModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ExportDto
+{
+    [XmlType("Project")]
+    public class ProjectXmlExportModel
+    {
+        [XmlAttribute("TasksCount")]
+        public int TasksCount { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public string HasEndDate { get; set; }
+
+        [XmlArray("Tasks")]
+        [XmlArrayItem("Task")]
+        public ProjectTaskXmlExportModel[] Tasks { get; set; }
+    }
+}
