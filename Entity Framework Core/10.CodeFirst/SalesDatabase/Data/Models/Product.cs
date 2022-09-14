57d7b991b1fa0d36f447b6dca1d9a4802a794b7c
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-14 16:46:32 +0300

    Add files via upload

---
+++ Entity Framework Core/10.CodeFirst/SalesDatabase/Data/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            Sales = new HashSet<Sale>();
+        }
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public double Quantity  { get; set; }
+        public decimal Price { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; }
+
+        [MaxLength(250)]
+        public string Description { get; set; }
+    }
+}
