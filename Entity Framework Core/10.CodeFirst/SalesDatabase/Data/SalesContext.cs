1fb2044b1fc764c668792f9829dab68973eeeba1
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-14 16:47:14 +0300

    Add files via upload

---
+++ Entity Framework Core/10.CodeFirst/SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+
+        }
+        public SalesContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Sale> Sales { get; set; }
+        public virtual DbSet<Store> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Integrated Security=True;Database=Sales");
+            };
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(x => x.Description)
+                .HasDefaultValue("No description");
+
+            modelBuilder.Entity<Sale>()
+                .Property(x => x.Date)
+                .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
