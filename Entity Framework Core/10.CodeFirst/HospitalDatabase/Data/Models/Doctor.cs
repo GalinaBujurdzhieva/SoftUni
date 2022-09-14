34b16cf7c1136f7af8785d2071abea2fb42103e1
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-14 16:44:36 +0300

    Add files via upload

---
+++ Entity Framework Core/10.CodeFirst/HospitalDatabase/Data/Models/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Doctor
+    {
+        public Doctor()
+        {
+            Visitations = new HashSet<Visitation>();
+        }
+        public int DoctorId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Specialty { get; set; }
+        public ICollection<Visitation> Visitations { get; set; }
+    }
+}
---
+++ Entity Framework Core/10.CodeFirst/HospitalDatabase/Data/Models/Patient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Patient
+    {
+        public Patient()
+        {
+            Visitations = new HashSet<Visitation>();
+            Diagnoses = new HashSet<Diagnose>();
+            Prescriptions = new HashSet<PatientMedicament>();
+        }
+        public int PatientId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Email { get; set; }
+
+        public bool HasInsurance { get; set; }
+        public ICollection<Visitation> Visitations { get; set; }
+        public ICollection<Diagnose> Diagnoses { get; set; }
+        public ICollection<PatientMedicament> Prescriptions { get; set; }
+    }
+}
