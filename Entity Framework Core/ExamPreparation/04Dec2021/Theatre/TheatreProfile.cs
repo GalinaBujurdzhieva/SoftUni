8cfcc236fe7cba6e2206252f9ca3a1f61a46ffb0
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-18 23:17:54 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/04Dec2021/Theatre/TheatreProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace Theatre
+{
+    using AutoMapper;
+    using Theatre.Data.Models;
+    using Theatre.DataProcessor.ImportDto;
+
+    class TheatreProfile : Profile
+    {
+        // Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE THIS CLASS
+        public TheatreProfile()
+        {
+            this.CreateMap<PlayXmlImportModel, Play>();
+            this.CreateMap<CastXmlImportModel, Cast>();
+        }
+    }
+}
