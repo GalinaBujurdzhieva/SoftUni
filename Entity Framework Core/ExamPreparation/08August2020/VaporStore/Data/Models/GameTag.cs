057bb7d45e6fc88657c76ae997b9c41e197eabe5
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-18 23:20:11 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/08August2020/VaporStore/Data/Models/GameTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaporStore.Data.Models
+{
+    public class GameTag
+    {
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+        public int TagId { get; set; }
+        public virtual Tag Tag { get; set; }
+    }
+}
---
+++ Entity Framework Core/ExamPreparation/08August2020/VaporStore/Data/Models/Purchase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public PurchaseType Type { get; set; }
+
+        [Required]
+        [MaxLength(14)]
+        public string ProductKey { get; set; }
+        public DateTime Date { get; set; }
+        public int CardId { get; set; }
+        public virtual Card Card { get; set; }
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+    }
+}
