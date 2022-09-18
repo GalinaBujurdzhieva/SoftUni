51bfb076e8daaafc5fa8faca66424cea6d2317c2
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-18 23:12:37 +0300

    Create CastXmlImportModel.cs

---
+++ Entity Framework Core/ExamPreparation/04Dec2021/Theatre/DataProcessor/ImportDto/CastXmlImportModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType("Cast")]
+    public class CastXmlImportModel
+    {
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string FullName { get; set; }
+        public bool IsMainCharacter { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        [RegularExpression(@"\+44-[0-9]{2}-[0-9]{3}-[0-9]{4}")]
+        public string PhoneNumber { get; set; }
+
+        public int PlayId { get; set; }
+    }
+}
