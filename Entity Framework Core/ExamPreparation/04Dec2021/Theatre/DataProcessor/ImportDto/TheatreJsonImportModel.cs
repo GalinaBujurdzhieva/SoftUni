5334b08f17d72848130470375e5269daa720f1b9
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-18 23:12:48 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/04Dec2021/Theatre/DataProcessor/ImportDto/TheatreJsonImportModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    public class TheatreJsonImportModel
+    {
+        public TheatreJsonImportModel()
+        {
+            Tickets = new List<TicketJsonImportModel>();
+        }
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Range(1, 10)]
+        public sbyte NumberOfHalls { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string Director { get; set; }
+        public List<TicketJsonImportModel> Tickets { get; set; }
+    }
+}
