24a7013ee2fe70bd2775c1e0a1e4d4bb8c76d668
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-18 23:21:41 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/08August2020/VaporStore/DataProcessor/Dto/ExportModels/GameXmlExportModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.Dto.Export
+{
+    [XmlType("Game")]
+    public class GameXmlExportModel
+    {
+        [XmlAttribute("title")]
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
