8619bde6436e10e50231c0d734954cdee1ac5d7e
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-18 23:09:58 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/04Dec2021/Theatre/Data/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Theatre.Data.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        public decimal Price { get; set; }
+        public sbyte RowNumber { get; set; }
+        public int PlayId { get; set; }
+        public virtual Play Play { get; set; }
+        public int TheatreId { get; set; }
+        public virtual Theatre Theatre { get; set; }
+    }
+}
