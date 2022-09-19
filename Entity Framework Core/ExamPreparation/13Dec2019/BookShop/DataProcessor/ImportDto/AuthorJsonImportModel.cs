b4ea69f6e3273a08a05fa3d1dab975141fb73b6c
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-19 17:11:27 +0300

    Create AuthorJsonImportModel.cs

---
+++ Entity Framework Core/ExamPreparation/13Dec2019/BookShop/DataProcessor/ImportDto/AuthorJsonImportModel.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    public class AuthorJsonImportModel
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"[0-9]{3}-[0-9]{3}-[0-9]{4}")]
+        public string Phone { get; set; }
+
+      
+        public AuthorsBooksJsonImportModel[] Books { get; set; }
+    }
+}
