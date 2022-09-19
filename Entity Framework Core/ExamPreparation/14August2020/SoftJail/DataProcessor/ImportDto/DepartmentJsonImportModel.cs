071390d5f212ed68de99d82309ebc6e4affb5f43
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-19 17:27:50 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/14August2020/SoftJail/DataProcessor/ImportDto/DepartmentJsonImportModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class DepartmentJsonImportModel
+    {
+        public DepartmentJsonImportModel()
+        {
+            Cells = new List<CellDepartmentJsonImportModel>();
+        }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        public List<CellDepartmentJsonImportModel> Cells { get; set; }
+    }
+}
---
+++ Entity Framework Core/ExamPreparation/14August2020/SoftJail/DataProcessor/ImportDto/OfficerXmlImportModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+     public class OfficerXmlImportModel
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Money { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+
+        [Required]
+        public string Weapon { get; set; }
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public PrisonerOfficerXmlImportModel[] Prisoners { get; set; }
+    }
+}
