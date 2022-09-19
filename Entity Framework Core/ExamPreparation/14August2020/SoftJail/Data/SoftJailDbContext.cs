3e1f1245eb676c1e6f147d19a515c4aee20d0ffb
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-19 17:22:27 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/14August2020/SoftJail/Data/SoftJailDbContext.cs
@@ -0,0 +1,40 @@
+﻿namespace SoftJail.Data
+{
+	using Microsoft.EntityFrameworkCore;
+    using SoftJail.Data.Models;
+
+    public class SoftJailDbContext : DbContext
+	{
+		public SoftJailDbContext()
+		{
+		}
+
+		public SoftJailDbContext(DbContextOptions options)
+			: base(options)
+		{
+		}
+
+        public virtual DbSet<Cell> Cells { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Mail> Mails { get; set; }
+        public virtual DbSet<Officer> Officers { get; set; }
+        public virtual DbSet<OfficerPrisoner> OfficersPrisoners { get; set; }
+        public virtual DbSet<Prisoner> Prisoners { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder
+					.UseSqlServer(Configuration.ConnectionString);
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.Entity<OfficerPrisoner>()
+				.HasKey(k => new { k.OfficerId, k.PrisonerId });
+		}
+	}
+}
