f47852708e6b45b2f6158250eeb16cea70c8e8a8
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-19 17:18:07 +0300

    Create Position.cs

---
+++ Entity Framework Core/ExamPreparation/14August2020/SoftJail/Data/Models/Enums/Position.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftJail.Data.Models.Enums
+{
+    public enum Position
+    {
+        Overseer, 
+        Guard, 
+        Watcher, 
+        Labour
+    }
+}
