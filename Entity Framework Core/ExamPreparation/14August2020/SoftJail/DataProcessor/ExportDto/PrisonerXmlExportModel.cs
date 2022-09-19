b6bb0c7d03722ab63a276f905ad88caefaa1d745
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-19 17:24:32 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/14August2020/SoftJail/DataProcessor/ExportDto/PrisonerXmlExportModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ExportDto
+{
+    [XmlType("Prisoner")]
+    public class PrisonerXmlExportModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public string IncarcerationDate { get; set; }
+
+        [XmlArray("EncryptedMessages")]
+        public MailPrisonerXmlExportModel[] EncryptedMessages { get; set; }
+    }
+}
