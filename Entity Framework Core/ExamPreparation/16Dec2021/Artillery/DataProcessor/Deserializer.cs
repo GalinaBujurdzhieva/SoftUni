73a1869885084adff1637352d492de834eadbf16
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-20 14:15:47 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/16Dec2021/Artillery/DataProcessor/Deserializer.cs
@@ -0,0 +1,179 @@
+﻿namespace Artillery.DataProcessor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Artillery.Data;
+    using Artillery.Data.Models;
+    using Artillery.Data.Models.Enums;
+    using Artillery.DataProcessor.ImportDto;
+    using AutoMapper;
+    using Newtonsoft.Json;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage =
+                "Invalid data.";
+        private const string SuccessfulImportCountry =
+            "Successfully import {0} with {1} army personnel.";
+        private const string SuccessfulImportManufacturer =
+            "Successfully import manufacturer {0} founded in {1}.";
+        private const string SuccessfulImportShell =
+            "Successfully import shell caliber #{0} weight {1} kg.";
+        private const string SuccessfulImportGun =
+            "Successfully import gun {0} with a total weight of {1} kg. and barrel length of {2} m.";
+        
+        static IMapper mapper;
+
+        public static string ImportCountries(ArtilleryContext context, string xmlString)
+        {
+            const string rootElement = "Countries";
+            var serializer = new XmlSerializer(typeof(List<CountryXmlImportModel>), new XmlRootAttribute(rootElement));
+            var textReader = new StringReader(xmlString);
+            var countriesDTO = serializer.Deserialize(textReader) as List<CountryXmlImportModel>;
+            
+            List<Country> validCountries = new List<Country>();
+            StringBuilder sb = new StringBuilder();
+            InitializeMapper();
+
+            foreach (var countryDTO in countriesDTO)
+            {
+                if (!IsValid(countryDTO))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                var currentValidCountry = mapper.Map<Country>(countryDTO);
+                validCountries.Add(currentValidCountry);
+                sb.AppendLine(string.Format(SuccessfulImportCountry, currentValidCountry.CountryName, currentValidCountry.ArmySize));
+            }
+            context.Countries.AddRange(validCountries);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportManufacturers(ArtilleryContext context, string xmlString)
+        {
+            const string rootElement = "Manufacturers";
+            var serializer = new XmlSerializer(typeof(List<ManufacturerXmlImportModel>), new XmlRootAttribute(rootElement));
+            var textReader = new StringReader(xmlString);
+            var manufacturersDTO = serializer.Deserialize(textReader) as List<ManufacturerXmlImportModel>;
+
+            List<Manufacturer> validManufacturers = new List<Manufacturer>();
+            StringBuilder sb = new StringBuilder();
+            InitializeMapper();
+
+            foreach (var manufacturerDTO in manufacturersDTO)
+            {
+                bool isManufacturerNotUnique = validManufacturers.Any(x => x.ManufacturerName == manufacturerDTO.ManufacturerName);
+                
+                if (!IsValid(manufacturerDTO) || isManufacturerNotUnique)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                var currentValidManufacturer = mapper.Map<Manufacturer>(manufacturerDTO);
+                validManufacturers.Add(currentValidManufacturer);
+                var currentValidManufacturerFoundedDetails = currentValidManufacturer.Founded.Split(", ").ToArray();
+                var currentValidManufacturerCityAndCountry = currentValidManufacturerFoundedDetails.Skip(currentValidManufacturerFoundedDetails.Length - 2).ToArray();
+
+                sb.AppendLine(string.Format(SuccessfulImportManufacturer, currentValidManufacturer.ManufacturerName, string.Join(", ", currentValidManufacturerCityAndCountry)));
+            }
+            context.Manufacturers.AddRange(validManufacturers);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportShells(ArtilleryContext context, string xmlString)
+        {
+            const string rootElement = "Shells";
+            var serializer = new XmlSerializer(typeof(List<ShellXmlImportModel>), new XmlRootAttribute(rootElement));
+            var textReader = new StringReader(xmlString);
+            var shellsDTO = serializer.Deserialize(textReader) as List<ShellXmlImportModel>;
+
+            List<Shell> validShells = new List<Shell>();
+            StringBuilder sb = new StringBuilder();
+            InitializeMapper();
+
+            foreach (var shellDTO in shellsDTO)
+            {
+                if (!IsValid(shellDTO))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                var currentValidShell = mapper.Map<Shell>(shellDTO);
+                validShells.Add(currentValidShell);
+                sb.AppendLine(string.Format(SuccessfulImportShell, currentValidShell.Caliber, currentValidShell.ShellWeight));
+            }
+            context.Shells.AddRange(validShells);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportGuns(ArtilleryContext context, string jsonString)
+        {
+            var gunsWithCountriesDTO = JsonConvert.DeserializeObject<IEnumerable<GunJsonImportModel>>(jsonString);
+            List<Gun> validGuns = new List<Gun>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var gunWithCountryDTO in gunsWithCountriesDTO)
+            {
+                bool isCurrentDTOEnumValid = Enum.TryParse(gunWithCountryDTO.GunType, out GunType gunType);
+                
+                if (!IsValid(gunWithCountryDTO) || !isCurrentDTOEnumValid)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                var currentGunWithCountry = new Gun
+                {
+                    ManufacturerId = gunWithCountryDTO.ManufacturerId,
+                    GunWeight = gunWithCountryDTO.GunWeight,
+                    BarrelLength = gunWithCountryDTO.BarrelLength,
+                    NumberBuild = gunWithCountryDTO.NumberBuild,
+                    Range = gunWithCountryDTO.Range,
+                    GunType = gunType,
+                    ShellId = gunWithCountryDTO.ShellId
+                };
+                foreach (var countryDto in gunWithCountryDTO.Countries)
+                {
+                    var currentGunCountry = context.Countries.FirstOrDefault(x => x.Id == countryDto.Id);
+
+                    currentGunWithCountry.CountriesGuns.Add(new CountryGun
+                    {
+                        Gun = currentGunWithCountry,
+                        Country = currentGunCountry
+                    });
+                }
+                validGuns.Add(currentGunWithCountry);
+                sb.AppendLine(string.Format(SuccessfulImportGun, currentGunWithCountry.GunType, currentGunWithCountry.GunWeight, currentGunWithCountry.BarrelLength));
+            }
+            context.Guns.AddRange(validGuns);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+        private static bool IsValid(object obj)
+        {
+            var validator = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationRes = new List<ValidationResult>();
+
+            var result = Validator.TryValidateObject(obj, validator, validationRes, true);
+            return result;
+        }
+        private static void InitializeMapper()
+        {
+            var configuration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<ArtilleryProfile>();
+            });
+            mapper = configuration.CreateMapper();
+        }
+    }
+
+    
+}
---
+++ Entity Framework Core/ExamPreparation/16Dec2021/Artillery/DataProcessor/Serializer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Artillery.DataProcessor
+{
+    using Artillery.Data;
+    using Artillery.Data.Models.Enums;
+    using Artillery.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+
+    public class Serializer
+    {
+        public static string ExportShells(ArtilleryContext context, double shellWeight)
+        {
+            var shellsToBeExported = context.Shells
+                .Where(x => x.ShellWeight > shellWeight)
+                .OrderBy(x => x.ShellWeight)
+                .Select(x => new
+                {
+                    ShellWeight = x.ShellWeight,
+                    Caliber = x.Caliber,
+                    Guns = x.Guns
+                    .Where(g => g.GunType == GunType.AntiAircraftGun)
+                    .OrderByDescending(g => g.GunWeight)
+                    .Select(g => new
+                    {
+                        GunType = g.GunType.ToString(),
+                        GunWeight = g.GunWeight,
+                        BarrelLength = g.BarrelLength,
+                        Range = g.Range > 3000 ? "Long-range" : "Regular range"
+                    }).ToList()
+
+                }).ToList();
+
+            return JsonConvert.SerializeObject(shellsToBeExported, Formatting.Indented);
+        }
+
+        public static string ExportGuns(ArtilleryContext context, string manufacturer)
+        {
+            var gunsWithCountries = context.Guns
+                .Where(x => x.Manufacturer.ManufacturerName == manufacturer)
+                .OrderBy(x => x.BarrelLength)
+                .Select(x => new GunWithCountryXmlExportModel
+                {
+                    Manufacturer = x.Manufacturer.ManufacturerName,
+                    GunType = x.GunType.ToString(),
+                    GunWeight = x.GunWeight,
+                    BarrelLength = x.BarrelLength,
+                    Range = x.Range,
+                    Countries = x.CountriesGuns
+                           .Where(x => x.Country.ArmySize > 4500000)
+                           .OrderBy(c => c.Country.ArmySize)
+                           .Select(c => new CountryXmlExportModel
+                                   {
+                                       CountryName = c.Country.CountryName,
+                                       ArmySize = c.Country.ArmySize
+                                   }).ToArray()
+                }).ToArray();
+
+            const string rootElement = "Guns";
+            var serializer = new XmlSerializer(typeof(GunWithCountryXmlExportModel[]), new XmlRootAttribute(rootElement));
+            var textWriter = new StringWriter();
+            using (textWriter)
+            {
+                serializer.Serialize(textWriter, gunsWithCountries, GetXmlNamespaces());
+            }
+            return textWriter.ToString();
+        }
+        private static XmlSerializerNamespaces GetXmlNamespaces()
+        {
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+            return xmlNamespaces;
+        }
+    }
+}
