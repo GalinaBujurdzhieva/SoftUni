699d9346422f399b18b9f16045dd16613c744930
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-20 14:12:39 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/16Dec2021/Artillery/Data/Models/Manufacturer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Artillery.Data.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer()
+        {
+            Guns = new HashSet<Gun>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string ManufacturerName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Founded { get; set; }
+
+        public virtual ICollection<Gun> Guns { get; set; }
+    }
+}
---
+++ Entity Framework Core/ExamPreparation/16Dec2021/Artillery/Data/Models/Shell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Artillery.Data.Models
+{
+    public class Shell
+    {
+        public Shell()
+        {
+            Guns = new HashSet<Gun>();
+        }
+        public int Id { get; set; }
+        public double ShellWeight { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Caliber { get; set; }
+
+        public virtual ICollection<Gun> Guns { get; set; }
+    }
+}
