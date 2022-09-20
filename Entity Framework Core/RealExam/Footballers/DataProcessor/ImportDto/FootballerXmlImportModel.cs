f74c474437e97d7b294798fbeec9b7b64dd87a81
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-20 14:24:53 +0300

    Add files via upload

---
+++ Entity Framework Core/RealExam/Footballers/DataProcessor/ImportDto/FootballerXmlImportModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+    public class FootballerXmlImportModel
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Nationality { get; set; }
+
+        [XmlArray("Footballers")]
+        public FootballerCoachXmlImportModel[] Footballers { get; set; }
+    }
+}
---
+++ Entity Framework Core/RealExam/Footballers/DataProcessor/ImportDto/TeamJsonImportModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    public class TeamJsonImportModel
+    {
+        
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        [RegularExpression(@"^[\w\s.-]{1,}$")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 2)]
+        public string Nationality { get; set; }
+
+        public int Trophies { get; set; }
+        public IEnumerable<int> Footballers { get; set; }
+    }
+}
