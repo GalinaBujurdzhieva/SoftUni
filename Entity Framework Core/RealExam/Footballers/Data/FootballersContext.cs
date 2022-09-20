217c9073650b158e8f8f8cb46a66fbe8ba0cdb63
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-20 14:22:27 +0300

    Add files via upload

---
+++ Entity Framework Core/RealExam/Footballers/Data/FootballersContext.cs
@@ -0,0 +1,33 @@
+﻿namespace Footballers.Data
+{
+    using Footballers.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class FootballersContext : DbContext
+    {
+        public FootballersContext() { }
+
+        public FootballersContext(DbContextOptions options)
+            : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        public virtual DbSet<Coach> Coaches { get; set; }
+        public virtual DbSet<Footballer> Footballers { get; set; }
+        public virtual DbSet<TeamFootballer> TeamsFootballers { get; set; }
+        public virtual DbSet<Team> Teams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TeamFootballer>()
+                .HasKey(x => new { x.FootballerId, x.TeamId });
+        }
+    }
+}
