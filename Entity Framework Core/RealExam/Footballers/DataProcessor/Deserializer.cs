f235c32c3b2abca5cae68ed7de49fb1773059e7d
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-20 14:25:13 +0300

    Add files via upload

---
+++ Entity Framework Core/RealExam/Footballers/DataProcessor/Deserializer.cs
@@ -0,0 +1,152 @@
+﻿namespace Footballers.DataProcessor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Data;
+    using Footballers.Data.Models;
+    using Footballers.Data.Models.Enums;
+    using Footballers.DataProcessor.ImportDto;
+    using Newtonsoft.Json;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedCoach
+            = "Successfully imported coach - {0} with {1} footballers.";
+
+        private const string SuccessfullyImportedTeam
+            = "Successfully imported team - {0} with {1} footballers.";
+
+        public static string ImportCoaches(FootballersContext context, string xmlString)
+        {
+            const string rootElement = "Coaches";
+            var serializer = new XmlSerializer(typeof(List<FootballerXmlImportModel>), new XmlRootAttribute(rootElement));
+            var textReader = new StringReader(xmlString);
+            var coachesDtos = serializer.Deserialize(textReader) as List<FootballerXmlImportModel>;
+
+            var validCoaches = new List<Coach>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var coachDto in coachesDtos)
+            {
+                if (!IsValid(coachDto) || string.IsNullOrEmpty(coachDto.Nationality))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                DateTime invalidNationality;
+                if (DateTime.TryParse(coachDto.Nationality, out invalidNationality))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var currentCoach = new Coach
+                {
+                    Name = coachDto.Name,
+                    Nationality = coachDto.Nationality
+                };
+                foreach (var footballerDto in coachDto.Footballers)
+                {
+                    DateTime currentFootballerContractStartDate;
+                    var isCurrentFootballerContractStartDateValid = DateTime.TryParseExact(footballerDto.ContractStartDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out currentFootballerContractStartDate);
+
+                    DateTime currentFootballerContractEndDate;
+                    var isCurrentFootballerContractEndDateValid = DateTime.TryParseExact(footballerDto.ContractEndDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out currentFootballerContractEndDate);
+
+                    bool isCurrentDTOBestSkillTypeValid = Enum.TryParse(footballerDto.BestSkillType, out BestSkillType bestSkillType);
+                    bool isCurrentDTOPositionTypeValid = Enum.TryParse(footballerDto.PositionType, out PositionType positionType);
+
+                    if (!IsValid(footballerDto)
+                        || !isCurrentFootballerContractStartDateValid
+                        || !isCurrentFootballerContractEndDateValid
+                        || !isCurrentDTOBestSkillTypeValid
+                        || !isCurrentDTOPositionTypeValid)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    if (currentFootballerContractStartDate > currentFootballerContractEndDate)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+                    currentCoach.Footballers.Add(new Footballer
+                    {
+                        Name = footballerDto.Name,
+                        ContractStartDate = currentFootballerContractStartDate,
+                        ContractEndDate = currentFootballerContractEndDate,
+                        PositionType = positionType,
+                        BestSkillType = bestSkillType
+                    });
+                }
+                validCoaches.Add(currentCoach);
+                sb.AppendLine(string.Format(SuccessfullyImportedCoach, currentCoach.Name, currentCoach.Footballers.Count));
+
+            }
+            context.Coaches.AddRange(validCoaches);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+        public static string ImportTeams(FootballersContext context, string jsonString)
+        {
+            var teamsDtos = JsonConvert.DeserializeObject<IEnumerable<TeamJsonImportModel>>(jsonString);
+            
+            var validTeams = new List<Team>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var teamDto in teamsDtos)
+            {
+                if (!IsValid(teamDto) || teamDto.Trophies <= 0)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                var currentTeam = new Team
+                {
+                    Name = teamDto.Name,
+                    Nationality = teamDto.Nationality,
+                    Trophies = teamDto.Trophies
+                };
+
+                var footballersIdsInDatabase = context.Footballers.Select(x => x.Id).ToList();
+                
+                foreach (var uniqueFootballerId in teamDto.Footballers.Distinct())
+                {
+                    
+                    if (!footballersIdsInDatabase.Contains(uniqueFootballerId))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+                    currentTeam.TeamsFootballers.Add(new TeamFootballer
+                    {
+                        Team = currentTeam,
+                        FootballerId = uniqueFootballerId
+                    });
+                }
+                validTeams.Add(currentTeam);
+                sb.AppendLine(string.Format(SuccessfullyImportedTeam, currentTeam.Name, currentTeam.TeamsFootballers.Count));
+            }
+            context.AddRange(validTeams);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ Entity Framework Core/RealExam/Footballers/DataProcessor/Serializer.cs
@@ -0,0 +1,84 @@
+﻿namespace Footballers.DataProcessor
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+    using Data;
+    using Footballers.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+        public static string ExportCoachesWithTheirFootballers(FootballersContext context)
+        {
+            var coachesWithFootballers = context.Coaches
+                .ToArray()
+                .Where(x => x.Footballers.Count >= 1)
+                .Select(x => new CoachXmlExportModel
+                {
+                    FootballersCount = x.Footballers.Count,
+                    CoachName = x.Name,
+                    Footballers = x.Footballers
+                    .Select(f => new FootballerCoachXmlExportModel
+                    {
+                        Name = f.Name,
+                        Position = f.PositionType.ToString()
+                    })
+                    .OrderBy(f => f.Name)
+                    .ToArray()
+                })
+                .OrderByDescending(x => x.FootballersCount)
+                .ThenBy(x => x.CoachName)
+                .ToArray();
+
+            const string rootElement = "Coaches";
+            var serializer = new XmlSerializer(typeof(CoachXmlExportModel[]), new XmlRootAttribute(rootElement));
+            var textWriter = new StringWriter();
+            using (textWriter)
+            {
+                serializer.Serialize(textWriter, coachesWithFootballers, GetXmlNamespaces());
+            }
+            return textWriter.ToString();
+        }
+
+        public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
+        {
+            var teamsWithFootballers = context.Teams
+                .ToList()
+                .Where(x => x.TeamsFootballers.Any(x => x.Footballer.ContractStartDate >= date))
+                .Select(x => new
+                {
+                    Name = x.Name,
+                    Footballers = x.TeamsFootballers
+                    .Where(f => f.Footballer.ContractStartDate >= date)
+                    .OrderByDescending(f => f.Footballer.ContractEndDate)
+                    .ThenBy(f => f.Footballer.Name)
+                    .Select(f => new
+                    {
+                        FootballerName = f.Footballer.Name,
+                        ContractStartDate = f.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture),
+                        ContractEndDate = f.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
+                        BestSkillType = f.Footballer.BestSkillType.ToString(),
+                        PositionType = f.Footballer.PositionType.ToString()
+                    }).ToList()
+                })
+                .OrderByDescending(x => x.Footballers.Count)
+                .ThenBy(x => x.Name)
+                .Take(5)
+                .ToList();
+
+            return JsonConvert.SerializeObject(teamsWithFootballers, Formatting.Indented);
+        }
+
+        private static XmlSerializerNamespaces GetXmlNamespaces()
+        {
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+            return xmlNamespaces;
+        }
+
+    }
+}
