46fb2f466595ee8c02d9dde111727049575bd59f
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2022-09-20 14:20:03 +0300

    Add files via upload

---
+++ Entity Framework Core/ExamPreparation/16Dec2021/Artillery/ArtilleryProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace Artillery
+{
+    using Artillery.Data.Models;
+    using Artillery.DataProcessor.ImportDto;
+    using AutoMapper;
+    class ArtilleryProfile : Profile
+    {
+        // Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE THIS CLASS
+        public ArtilleryProfile()
+        {
+            this.CreateMap<CountryXmlImportModel, Country>();
+            this.CreateMap<ManufacturerXmlImportModel, Manufacturer>();
+            this.CreateMap<ShellXmlImportModel, Shell>();
+        }
+    }
+}
