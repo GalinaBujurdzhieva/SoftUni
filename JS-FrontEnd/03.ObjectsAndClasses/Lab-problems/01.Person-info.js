213a42079c0230af2e23040dee50e1f5d0b7d05e
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2023-03-16 10:09:26 +0200

    Create 01.Person-info.js

---
+++ JS-FrontEnd/03.ObjectsAndClasses/Lab-problems/01.Person-info.js
@@ -0,0 +1,10 @@
+function personInfo(firstName, lastName, age){
+    const person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    }
+    return person;
+}
+console.log(personInfo("Peter", "Pan", "20"));
+console.log(personInfo("George", "Smith","18"));
