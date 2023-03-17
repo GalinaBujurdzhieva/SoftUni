21e811ab20f04dae1e3e923e000cb9fb69d0a9ea
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2023-03-17 16:32:16 +0200

    Add files via upload

---
+++ JS-FrontEnd/04.DOM-and-Events/Lab/02.Show-more/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const elementToBeHidden = document.getElementById('more');
+    elementToBeHidden.style.display = 'none';
+    const elementToBeShown = document.getElementById('text');
+    elementToBeShown.style.display = 'inline';
+}
